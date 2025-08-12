10a7ae2a5d5379eed8272257e65f84cf140184eb
A voz que clama no deserto <devedu7957@gmail.com>
2025-08-12 15:28:51 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+var data  = new Date(); // Cria um objeto Date com a data e hora atuais
+var dia= data.getDate();
+var mes = data.getMonth();
+var ano = data.getFullYear();
+
+
+
+var hoje=dia+"/"+mes+"/"+ano
+export {hoje}
+export {dia}
+
