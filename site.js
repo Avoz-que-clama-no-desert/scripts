0189edded33e07af7dc62f3deb6324808affa1b7
A voz que clama no deserto <devedu7957@gmail.com>
2025-08-14 18:09:59 -0300

    Update site.js

--- site.js
+++ site.js
@@ -3,38 +3,7 @@
      import {parasha}from './parasha.js'
      import {audio}from './audio.js'
 
-var menu = document.querySelector(".limenu")
-var canal1 =document.querySelector(".canal1")
-var canal =document.querySelector(".canal")
 
-
-
-menu.addEventListener("mouseover",()=>{
-    canal.style.display="block"
-    canal1.style.display="block"
-})
-menu.addEventListener("mouseout",()=>{
-    
-})
-
-canal.addEventListener("mouseover",()=>{
-    canal.style.display="block"
-    canal1.style.display="block" 
-})
-canal1.addEventListener("mouseover",()=>{
-    canal.style.display="block"
-    canal1.style.display="block" 
-})
-canal1.addEventListener("mouseout",()=>{
-    canal.style.display="none"
-    canal1.style.display="none"
-    
-})
-canal.addEventListener("mouseout",()=>{
-    canal.style.display="none"
-    canal1.style.display="none"
-    
-})
      
 
      document.querySelector(".mostradia").textContent=parasha[1]
@@ -107,3 +76,4 @@ canal.addEventListener("mouseout",()=>{
      
 
      
+
