fcc45b4f9da90e06299e2c7cd748c66b0bddbfa7
A voz que clama no deserto <devedu7957@gmail.com>
2025-08-14 18:05:14 -0300

    Update site.js
    adicionou menu

--- site.js
+++ site.js
@@ -2,6 +2,39 @@
      import {hoje}from'./data.js'
      import {parasha}from './parasha.js'
      import {audio}from './audio.js'
+
+var menu = document.querySelector(".limenu")
+var canal1 =document.querySelector(".canal1")
+var canal =document.querySelector(".canal")
+
+
+
+menu.addEventListener("mouseover",()=>{
+    canal.style.display="block"
+    canal1.style.display="block"
+})
+menu.addEventListener("mouseout",()=>{
+    
+})
+
+canal.addEventListener("mouseover",()=>{
+    canal.style.display="block"
+    canal1.style.display="block" 
+})
+canal1.addEventListener("mouseover",()=>{
+    canal.style.display="block"
+    canal1.style.display="block" 
+})
+canal1.addEventListener("mouseout",()=>{
+    canal.style.display="none"
+    canal1.style.display="none"
+    
+})
+canal.addEventListener("mouseout",()=>{
+    canal.style.display="none"
+    canal1.style.display="none"
+    
+})
      
 
      document.querySelector(".mostradia").textContent=parasha[1]
@@ -72,4 +105,5 @@
      document.querySelector(".p60").textContent=parasha[63]
      
      
+
      
